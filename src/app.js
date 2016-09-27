5d9adbd2df859248a4f24d1cd44074dbfe61bd8a
DanCLee <dan.clee1218@gmail.com>
2016-09-26 20:53:55 -0700

    Added redux storage to save ththe state

--- src/app.js
+++ src/app.js
@@ -3,17 +3,28 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-reactnativeasyncstorage';
 
 import * as reducers from './reducers';
 
 
 import BeeBabble from './beeBabble';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const reducer = combineReducers(reducers);
+
+//const reducer = combineReducers(reducers);
+const reducer = storage.reducer(combineReducers(reducers));
+const engine = createEngine('messengerBee1');
+const storageMiddleware = storage.createMiddleware(engine);
+//const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+let middleware = [thunk, storageMiddleware]
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 const store = createStoreWithMiddleware(reducer);
 
-
+const load = storage.createLoader(engine);
+load(store)
+	.then((newState) => console.log('Loaded state:', newState))
+	.catch(() => console.log('Failed to load previous state'));
 
 module.exports = React.createClass({
 
